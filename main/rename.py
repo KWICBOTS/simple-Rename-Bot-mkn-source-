470ec8c4af8b88d676ad0faae43edea251ad77fe
subham <118068354+student9173@users.noreply.github.com>
2023-01-16 15:24:36 +0530

    Update rename.py

--- main/rename.py
+++ main/rename.py
@@ -34,7 +34,7 @@ async def rename_file(bot, msg):
     await sts.edit("Trying to Upload...")
     c_time = time.time()
     try:
-        await bot.send_document(msg.chat.id, document=downloaded, thumb=og_thumbnail, caption=cap, progress=progress_message, progress_args=("Uploade Started.....", sts, c_time))        
+        await bot.send_document(msg.chat.id, document=downloaded, thumb=og_thumbnail, caption=cap, progress=progress_message, progress_args=("Uploading Started.....", sts, c_time))        
     except Exception as e:  
         await sts.edit(f"Error {e}") 
         return               
