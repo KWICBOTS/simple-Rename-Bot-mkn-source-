969f32a7ce1ccdc5d45b679481e2248ddb57a535
subham <118068354+student9173@users.noreply.github.com>
2023-01-16 09:21:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "22140808"))
+API_HASH = environ.get("API_HASH", "72e39b383c4655d957f67d4ab94cb3aa")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5922415187:AAHg9Q9HxjR3IhW_suIJcBVUKAgp_drK0qg")
+ADMIN = int(environ.get("ADMIN", "5763733318"))          
+CAPTION = environ.get("CAPTION", "join @MoviesNowV2")
 
 class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "")
+    THUMBNAIL = environ.get("THUMBNAIL", "AgACAgQAAxkBAAMOY8TEwbCt1DIeWlMgrMiDAilQ77wAAru6MRtdNSlSwfv7iocxQ_cACAEAAwIAA3gABx4E")
